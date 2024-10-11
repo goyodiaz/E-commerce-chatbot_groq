e7b05b5c592d1584886d6bf4485997e9809fea7c
Dohun Kim <32084214+dounykim@users.noreply.github.com>
2024-10-11 16:07:59 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -16,7 +16,7 @@ opik.configure(api_key=OPIK_API_KEY, workspace='wizard-bot1')
 
 # Comet Opik 프로젝트에 연결
 # Comet Opik 프로젝트와 연결하면 모델의 학습 과정이나 결과를 기록하고 관리할 수 있게 해준다.
-os.environ['OPIK_PROJECT_NAME'] = 'Wizard Chatbot Demo'  
+os.environ['OPIK_PROJECT_NAME'] = 'Wizard Chatbot Demo1'  
 
 # --- 마법 아이템 목록 ------------------------------------------------------------
 # 마법 상점에서 판매할 마법 아이템 목록을 문자열로 정의한다.
