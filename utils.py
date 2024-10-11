a82890cd94fdbea34fcbc786780b7cb766abd146
Dohun Kim <32084214+dounykim@users.noreply.github.com>
2024-10-11 14:02:35 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,6 +2,16 @@ import opik
 
 opik_client = opik.Opik()
 
-def opik_trace(user_input, response):
-    trace = opik_client.trace(name='chat', input={'user_input': user_input}, output={'response': response})
-    trace.span(name='llm_call', input={'context': user_input}, output={'response': response})
+def opik_trace(input_data, output, context):
+    trace = opik_client.trace(
+        name='chat',
+        input={'user_input': input_data},
+        output={'response': output}
+    )
+
+    trace.span(
+        name='llm_call',
+        type='llm',
+        input={'context': context},
+        output={'response': output}
+    )
