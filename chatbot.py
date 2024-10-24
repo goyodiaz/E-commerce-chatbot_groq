b1fb516c80f217b3260beff7ea2344d70a97f0b8
Goyo <goyodiaz@gmail.com>
2024-10-24 12:25:38 +0200

    Do not import utils.

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,20 @@
 import streamlit as st
-import utils
+import opik
+
+def opik_trace(input_data, output, context):
+    trace = opik_client.trace(
+        name='chat',
+        input={'user_input': input_data},
+        output={'response': output}
+    )
+
+    trace.span(
+        name='llm_call',
+        type='llm',
+        input={'context': context},
+        output={'response': output}
+    )
 
 if st.button("Make the call"):
-    utils.opik_trace("__PROMPT__", "__MSG__", [])
+    client = opik.Opik(project_name='Wizard Chatbot Demo1', workspace='wizard-bot1')
+    opik_trace("__PROMPT__", "__MSG__", [])
