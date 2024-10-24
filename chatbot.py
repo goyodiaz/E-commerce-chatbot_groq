969d7c5616c08e1f286c45b5bd0d8310b18dfbf1
Goyo <goyodiaz@gmail.com>
2024-10-24 13:06:08 +0200

    Examine configuration

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,14 @@
 import os
 import streamlit as st
 import opik
+import opik.config as config
 
 os.environ["OPIK_API_KEY"] = "123456"
+config_ = config.get_from_user_inputs(
+    project_name='Wizard Chatbot Demo1', workspace='wizard-bot1', url_override=None
+)
+st.write(config_)
+st.write(config_.api_key)
 client = opik.Opik(project_name='Wizard Chatbot Demo1', workspace='wizard-bot1')
 
 if st.button("Make the call"):
