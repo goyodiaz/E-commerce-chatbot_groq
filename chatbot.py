5f2d30181b823c02ed4f03faa184ceb92faf0e3b
Goyo <goyodiaz@gmail.com>
2024-10-24 12:26:35 +0200

    Fix client name.

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,7 @@ import streamlit as st
 import opik
 
 def opik_trace(input_data, output, context):
-    trace = opik_client.trace(
+    trace = client.trace(
         name='chat',
         input={'user_input': input_data},
         output={'response': output}
