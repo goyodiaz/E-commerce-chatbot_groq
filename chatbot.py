1e3ec8ab28a29dbe883f04a52eec0ac7be60ae23
Goyo <goyodiaz@gmail.com>
2024-10-24 12:48:08 +0200

    Get help on client_wrapper

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,7 @@ import streamlit as st
 import opik
 
 client = opik.Opik(project_name='Wizard Chatbot Demo1', workspace='wizard-bot1')
-st.help(client._rest_client)
+st.help(client._rest_client._client_wrapper)
 
 if st.button("Make the call"):
     trace = client.trace(
