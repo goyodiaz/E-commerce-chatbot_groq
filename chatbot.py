9029d80cb24a3e623021078777ce7058733970ab
Goyo <goyodiaz@gmail.com>
2024-10-24 12:36:06 +0200

    Get rid of the indirection.

--- chatbot.py
+++ chatbot.py
@@ -1,29 +1,19 @@
 import streamlit as st
 import opik
 
-def opik_trace(input_data, output, context):
-    trace = client.trace(
-        name='chat',
-        input={'user_input': input_data},
-        output={'response': output}
-    )
-
-    trace.span(
-        name='llm_call',
-        type='llm',
-        input={'context': context},
-        output={'response': output}
-    )
-
 client = opik.Opik(project_name='Wizard Chatbot Demo1', workspace='wizard-bot1')
 
 if st.button("Make the call"):
-    # opik_trace("__PROMPT__", "__MSG__", [])
-    input_data = "__PROMPT__"
-    output = "__MSG__"
-    context = []
     trace = client.trace(
         name='chat',
-        input={'user_input': input_data},
-        output={'response': output}
+        input={'user_input': "__PROMPT__"},
+        output={'response': "__MSG__"}
     )
+    ```
+    trace.span(
+        name='llm_call',
+        type='llm',
+        input={'context': []},
+        output={'response': "__MSG__"}
+    )
+    ```
