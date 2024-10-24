e91770a655ee5a853a46fbff82aa6ac6ff65e8e8
Goyo <goyodiaz@gmail.com>
2024-10-24 12:56:31 +0200

    Do not get help, define key in the environment.

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,9 @@
+import os
 import streamlit as st
 import opik
 
+os.environ["OPIK_API_KEY"] = "123456"
 client = opik.Opik(project_name='Wizard Chatbot Demo1', workspace='wizard-bot1')
-st.help(client._rest_client._client_wrapper.httpx_client)
 
 if st.button("Make the call"):
     trace = client.trace(
